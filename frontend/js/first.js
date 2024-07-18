38692988d8be8a9b534ed9e7b035944848562c53
DevKaushik0007 <kaushikdev786786@gmail.com>
2024-07-18 11:40:55 +0530

    aggregate and js

---
+++ frontend/js/first.js
@@ -0,0 +1,20 @@
+//let var and const
+// these are the three types to declare any variable in javascript
+// var: function scoped mutable
+// let: block scoped and mutable
+//  const: block scoped and immutable
+
+// mutable and immutable
+// mutable: can be changed
+// immutable: can't be changed
+// on the reference of re-assignment
+
+// what is function scope?
+// function scope is the scope of a variable that is declared inside a function
+// it is the scope of a variable that is declared inside a function
+
+// what is block scope?
+// block scope is the scope of a variable that is declared inside a block of code.
+// curly brackets creates block
+var a = 5;
+console.log(a);
