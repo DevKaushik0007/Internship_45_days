0326534aa28f200b1fc82b1d486d1dcce49f637b
DevKaushik0007 <kaushikdev786786@gmail.com>
2024-07-19 22:32:09 +0530

    Counter program project

---
+++ Projects/counterProgram/index.js
@@ -0,0 +1,17 @@
+const value = document.getElementById("value");
+const btnDecrease = document.getElementById("btnDecrease");
+const btnReset = document.getElementById("btnReset");
+const btnIncrease = document.getElementById("btnIncrease");
+let count = 0;
+btnDecrease.onclick = function(){
+    count--;
+    value.textContent = count;
+}
+btnReset.onclick = function(){
+    count = 0;
+    value.textContent = count;
+}
+btnIncrease.onclick = function(){
+    count++;
+    value.textContent = count;
+}
