bb5f4f1846041a5370b27fe5c3164f4251ad7b54
DevKaushik0007 <kaushikdev786786@gmail.com>
2024-07-21 21:59:01 +0530

    Project updated

---
+++ Projects/CGPA-SGPA/index.js
@@ -0,0 +1,5 @@
+const math = document.getElementById("math");
+let res = math.value;
+res = Number(res);
+const submit = document.getElementById("submit").value
+console.log(submit);
--- Projects/counterProgram/index.js
+++ Projects/counterProgram/index.js
@@ -3,15 +3,15 @@ const btnDecrease = document.getElementById("btnDecrease");
 const btnReset = document.getElementById("btnReset");
 const btnIncrease = document.getElementById("btnIncrease");
 let count = 0;
-btnDecrease.onclick = function(){
+btnDecrease.onclick = function () {
     count--;
     value.textContent = count;
 }
-btnReset.onclick = function(){
+btnReset.onclick = function () {
     count = 0;
     value.textContent = count;
 }
-btnIncrease.onclick = function(){
+btnIncrease.onclick = function () {
     count++;
     value.textContent = count;
 }
