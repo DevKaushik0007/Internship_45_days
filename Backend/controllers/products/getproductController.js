f14e1b7152a5df76f21b8219da9c563d095e9131
DevKaushik0007 <kaushikdev786786@gmail.com>
2024-07-27 11:08:29 +0530

    backend

--- Backend/controllers/products/getproductController.js
+++ Backend/controllers/products/getproductController.js
@@ -2,20 +2,81 @@ const productCollection = require("../../modules/productSchema");
 const mongoose = require("mongoose");
 const productcontroller = async (req, res) => {
     try {
-        const { id, category, sub_category, name } = req.params;
+        const { id, category, sub_category, name , rating,keyword} = req.params;
         let products;
         if (id ) {
             products = await productCollection.findById(id);
         }
+        // else if (sub_category) {
+        //     const searchsubcategory = sub_category.toLowerCase();
+        //     products = await productCollection.find({ sub_category: { $regex: new RegExp(searchsubcategory, "i") } });
+        // }
         else if (sub_category) {
             const searchsubcategory = sub_category.toLowerCase();
-            products = await productCollection.find({ sub_category: { $regex: new RegExp(searchsubcategory, "i") } });
+           
+    
+            if(req.path.includes("/lowtohigh")){
+                products = await productCollection
+                .find({
+                    sub_category:{$regex:new RegExp(searchsubcategory,"i")},
+                })
+                .sort({new_price:1});
+            } else if(req.path.includes("/hightolow")){
+                products =await productCollection
+                .find({
+                    sub_category:{$regex:new RegExp(searchsubcategory,"i")},
+                })
+                .sort({new_price:-1});
+            } else {
+                products = await productCollection.find({ sub_category: { $regex: new RegExp(searchsubcategory, "i") } });
+            }
+        
         }
 
         else if (name) {
             const searchname = name.toLowerCase();
-            products =  await productCollection.find({ name: { $regex: new RegExp(searchname, "i") } });
-        } else if(req.path.includes("/random")){
+            
+            if(req.path.includes("/asc")){
+            products =  await productCollection.find().sort({name:1})
+        }
+        else if((req.path.includes("/desc"))) {
+            products =  await productCollection.find().sort({name:-1})
+        }}
+
+        else if (keyword) {
+            const searchkeyword = keyword.toLowerCase();
+            products =  await productCollection.find({
+                $or:[
+                    {name:{$regex:new RegExp(searchname,"i")}},
+                    {sub_category: { $regex: new RegExp(searchsubcategory, "i") } },
+                    {category:{$regex:new RegExp(searchcategory,"i")}}
+                ],
+            });
+        }
+
+
+        // else if (name) {
+        //     const searchname = name.toLowerCase();
+        //     if(req.path.includes("/lowtohigh")){
+        //         products = await productCollection
+        //         .find({
+        //             name:{$regex:new RegExp(searchname,"i")},
+        //         })
+        //         .sort({new_price:1});
+        //     } else if(req.path.includes("/hightolow")){
+        //         products =await productCollection
+        //         .find({
+        //             name:{$regex:new RegExp(searchname,"i")},
+        //         })
+        //         .sort({new_price:-1});
+        //     } else {
+        //         products =  await productCollection.find({ name: { $regex: new RegExp(searchname, "i") } });
+        //     }
+        
+        // }
+        
+        
+        else if(req.path.includes("/random")){
             products = await productCollection.aggregate([{
                 $sample:{size:5}
             }]);
@@ -51,17 +112,27 @@ const productcontroller = async (req, res) => {
         else if(req.path.includes("/lowtohigh")){
             products = await productCollection.find().sort({new_price:1})
         }
+        else if(rating){
+            if(req.path.includes("/lowtohighrating")){
+            products = await productCollection.find().sort({rating:1})
+        }}
+        // else if(req.path.includes("/lowtohighrating")){
+        //     products = await productCollection.aggregate([{
+        //         $match: {
+        //             colors:"White"
+        //       }},         
+        // ])}
         // else if(req.path.includes("/avgRating")){
         //     products = await productCollection.aggregate([{
         //         $group:{
         //             _id:null,
         //             avgRating:{$avg:"$rating"},
         //         }},
-        //         {
-        //             $addFields: {
-        //                 avgRating: "$avgRating"
-        //             }
-        //         },
+                // {
+                //     $addFields: {
+                //         avgRating: "$avgRating"
+                //     }
+                // },
         //         {
         //             $match: {
         //                 $expr: {
